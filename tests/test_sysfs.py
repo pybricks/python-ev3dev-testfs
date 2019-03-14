9b200e080b4724d0c3d3cbd398fcff90894bc8d4
David Lechner <david@lechnology.com>
2019-03-13 20:12:10 -0500

    tests: add wait_for_mount() function
    This is better than time.sleep() for waiting until a filesystem is mounted

--- tests/test_sysfs.py
+++ tests/test_sysfs.py
@@ -18,6 +18,37 @@ def get_tmp_dir() -> str:
         os.rmdir(t)
 
 
+def wait_for_mount(mount_point: str, timeout: float = 0.5):
+    """Wait for the mount point to appear.
+
+    Parameters
+    ----------
+        mount_point
+            The absolute path to the mount point.
+        timeout
+            Timeout in seconds
+
+    Raises
+    ------
+        TimeoutError
+            If the `timeout` is reached before the mount point is seen.
+    """
+    DELAY = 0.01  # delay in seconds between checks
+    MAX_COUNT = timeout // DELAY
+    count = 0
+    while True:
+        with open('/proc/mounts', 'r') as f:
+            for line in f.readlines():
+                if not line:
+                    break
+                if line.find(mount_point) > -1:
+                    return
+            if count > MAX_COUNT:
+                raise TimeoutError('Waiting for mount took too long')
+            count += 1
+            time.sleep(DELAY)
+
+
 @contextmanager
 def get_proc(mount_point: str) -> Popen:
     args = [
@@ -28,12 +59,25 @@ def get_proc(mount_point: str) -> Popen:
         '-o', 'auto_unmount'
     ]
     p = Popen(args, stdin=PIPE, stdout=PIPE)
-    time.sleep(1)  # FIXME: how to wait for mount?
     try:
+        wait_for_mount(mount_point)
         yield p
     finally:
         p.terminate()
         p.wait()
+
+
+def test_wait_for_mount_timeout():
+    TIMEOUT = 0.25
+    timeout_error = False
+    start_time = time.monotonic()
+    try:
+        wait_for_mount('---------', timeout=TIMEOUT)
+    except TimeoutError:
+        timeout_error = True
+
+    assert timeout_error
+    assert time.monotonic() - start_time > TIMEOUT
 
 
 def test_ls_root():
