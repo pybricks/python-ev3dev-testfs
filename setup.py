628c63cd5741abe9eb3a1e29f18d65927846a562
David Lechner <david@lechnology.com>
2019-03-20 12:29:03 -0500

    setup.py: add namespace package

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name='ev3dev-testfs',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
+    namespace_packages=['ev3dev'],
     packages=['ev3dev.testfs'],
     setup_requires=['pytest-runner'],
     test_requires=['pytest'],
